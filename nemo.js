0c667998a697e1f3505779cf40c533599d7929ae
Ana Ceprnja <aceprnja@iun.edu>
2021-05-06 14:36:31 -0600

    different ways of doing things

---
+++ nemo.js
@@ -0,0 +1,33 @@
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'gill', 'hank']
+const large = new Array(100000).fill('nemo');
+
+function findNemo(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 'nemo') {
+            console.log('found nemo');
+        }
+    }
+}
+findNemo(everyone);
+
+//another way of writing the same thing
+
+const findNemo2 = array => {
+    array.forEach(i => {
+        if (fish === 'nemo')
+            console.log('found nemo!');
+    })
+
+}
+findNemo(everyone);
+//another way 
+
+const findNemo3 = array => {
+    for (let fish of array) {
+        if (fish === 'nemo') {
+            console.log('found nemo!');
+        }
+    }
+}
+findNemo(everyone);
---
+++ tweet.js
@@ -0,0 +1,10 @@
+// your boss asks you to make a button that finds a users oldest and newest tweet
+
+// find 1st, find last or nth item 
+
+//if the tweets are stored in an array 
+
+const array = ['hi', 'my', 'ana'];
+array[0]; //0(1)
+array[array.length - 1] //O(1)
+
