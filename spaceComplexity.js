99e3f1c705e83ee0bf32e4abefda95ad727f9024
Ana Ceprnja <aceprnja@iun.edu>
2021-05-06 14:21:21 -0600

    space complex

--- spaceComplexity.js
+++ spaceComplexity.js
@@ -5,14 +5,17 @@ function boooo(n) {      // takes input
 }
 boooo([1, 2, 3, 4, 5]) //will print out booo 5 times
 
+// ^ this function has a space complexity of 0(1)
+
 
 // #6 Space complexity O(n)
 function arrayOfHiNTimes(n) {
     var hiArray = [];
     for (let i = 0; i < n; i++) {
-        hiArray[i] = 'hi';
+        hiArray[i] = 'hi'; //as we are looping we fill it with hi
     }
     return hiArray;
 }
 
-arrayOfHiNTimes(6)
+arrayOfHiNTimes(6) // print 6 times
+//space complexity is 0(n)
