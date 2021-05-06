6ede0bbe0fb2d24893d1ba1f7cf1b4cad2573dc4
Ana Ceprnja <aceprnja@iun.edu>
2021-05-06 12:31:32 -0600

    box 2

---
+++ rule3.js
@@ -0,0 +1,16 @@
+// different terms for inputs
+
+function compressBoxesTwice(boxes, boxes2) {
+    boxes.forEach(function (boxes) {
+        console.log(boxes);
+    })
+
+    boxes.forEach(function (boxes) {
+        console.log(boxes);
+    })
+}
+
+// first 4 loop is going to depend on how big the first item or input is
+// the second for loop depends on how big the second input is --> in this case boxes2
+
+//the big o is --> O(a+b) aka --> box one plus box two
